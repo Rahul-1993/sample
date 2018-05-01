18dd6220d708930fd7c0ef7cdef0e95ec1facb29
Rahul Avale <rahulavale9@gmail.com>
2018-05-01 12:03:46 -0400

    Develop branch commit 2

---
+++ sample/ViewControllerAddedByJohn.swift
@@ -0,0 +1,38 @@
+//
+//  ViewControllerAddedByJohn.swift
+//  sample
+//
+//  Created by Rahul Avale on 5/1/18.
+//  Copyright © 2018 Rahul Avale. All rights reserved.
+//
+
+import UIKit
+
+class ViewControllerAddedByJohn: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        
+        
+        //A new view controller with a xib
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
