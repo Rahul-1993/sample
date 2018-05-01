3047831bed530edd55c95b20609bd81598f174c0
Rahul Avale <rahulavale9@gmail.com>
2018-05-01 11:57:41 -0400

    first commit

---
+++ sample/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  sample
+//
+//  Created by Rahul Avale on 5/1/18.
+//  Copyright © 2018 Rahul Avale. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        
+        // viewDidLoad is always going to be the first thing to run!
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+        
+        // Dont even know why this is here
+    }
+
+
+}
+
